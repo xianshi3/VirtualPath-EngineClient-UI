5b970c19cb349f3b0888b5b35de4cb7a1b37930d
fu_yf <1784257751@qq.com>
2024-12-28 20:01:23 +0800

    优化代码

--- VirtualPathCore/Views/MainView.axaml.cs
+++ VirtualPathCore/Views/MainView.axaml.cs
@@ -30,15 +30,22 @@ namespace VirtualPathCore.Views
             // 设置数据上下文
             DataContext = _viewModel;
 
-            // 注册第一个渲染器的事件处理程序
-            glRenderer1.OnLoad += () => { _drawingService1.Load(new object[] { glRenderer1, _viewModel }); };
-            glRenderer1.OnUpdate += _drawingService1.Update;
-            glRenderer1.OnRender += _drawingService1.Render;
-
-            // 注册第二个渲染器的事件处理程序
-            glRenderer2.OnLoad += () => { _drawingService2.Load(new object[] { glRenderer2, _viewModel }); };
-            glRenderer2.OnUpdate += _drawingService2.Update;
-            glRenderer2.OnRender += _drawingService2.Render;
+            // 注册渲染器的事件处理程序
+            RegisterRendererEvents(glRenderer1, _drawingService1);
+            RegisterRendererEvents(glRenderer2, _drawingService2);
         }
+
+        /// <summary>
+        /// 注册渲染器的事件处理程序
+        /// </summary>
+        /// <param name="renderer">渲染器实例</param>
+        /// <param name="drawingService">绘图服务实例</param>
+        private void RegisterRendererEvents(VirtualPathCore.Graphics.OpenGL.Renderer renderer, IDrawingService drawingService)
+        {
+            renderer.OnLoad += () => { drawingService.Load([renderer, _viewModel]); };
+            renderer.OnUpdate += drawingService.Update;
+            renderer.OnRender += drawingService.Render;
+        }
+
     }
 }
