970c6dcab983f8f8d79e80bc8197b61e90d5d044
fu_yf <1784257751@qq.com>
2024-12-28 21:25:14 +0800

    添加图形相关注释

--- VirtualPathCore/Contracts/Services/IDrawingService.cs
+++ VirtualPathCore/Contracts/Services/IDrawingService.cs
@@ -1,10 +1,25 @@
 ﻿namespace VirtualPathCore.Contracts.Services;
 
+/// <summary>
+/// 定义绘图服务的接口，用于加载资源、更新状态和渲染内容
+/// </summary>
 public interface IDrawingService
 {
+    /// <summary>
+    /// 加载绘图服务所需的资源
+    /// </summary>
+    /// <param name="args">加载资源时所需的参数数组</param>
     void Load(object[] args);
 
+    /// <summary>
+    /// 更新绘图服务的状态
+    /// </summary>
+    /// <param name="deltaSeconds">自上次更新以来的时间间隔（以秒为单位）</param>
     void Update(double deltaSeconds);
 
+    /// <summary>
+    /// 渲染绘图服务的内容
+    /// </summary>
+    /// <param name="deltaSeconds">自上次渲染以来的时间间隔（以秒为单位）</param>
     void Render(double deltaSeconds);
 }
